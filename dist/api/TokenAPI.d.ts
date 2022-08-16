7faf3471d9702d3c3b1913ebbdf764cbf4a2c8e7
openking <1315383301@qq.com>
2022-08-16 18:12:50 +0800

    feat:denom_meta_of_token1

--- dist/api/TokenAPI.d.ts
+++ dist/api/TokenAPI.d.ts
@@ -1,12 +1,19 @@
 import { BaseAPI } from "./BaseAPI";
-import { Metadata } from "../proto/cosmos/bank/v1beta1/bank";
+import { DenomUnit, Metadata } from "../proto/cosmos/bank/v1beta1/bank";
 import { MetadataExt } from "../proto/token/token";
-export interface QueryMetadataResponse {
-    metadata: Metadata;
-    metadataExt?: MetadataExt;
+export interface IMetadata extends Metadata {
+    denom_units: DenomUnit[];
+}
+export interface IMetadataExt extends MetadataExt {
+    max_supply: string;
+}
+interface QueryMetadataResponse {
+    metadata?: IMetadata;
+    metadata_ext?: IMetadataExt;
     supply: string;
 }
 export declare class TokenAPI extends BaseAPI {
     apiTokenMetadatasByDenom(denom: string): Promise<QueryMetadataResponse>;
     apiTokenMetadatas(): Promise<Array<Metadata>>;
 }
+export {};
--- src/api/TokenAPI.ts
+++ src/api/TokenAPI.ts
@@ -1,14 +1,28 @@
 import { BaseAPI } from "./BaseAPI";
-import { Metadata } from "../proto/cosmos/bank/v1beta1/bank";
+import {DenomUnit, Metadata} from "../proto/cosmos/bank/v1beta1/bank";
 import { QueryGetMetadataResponse } from "../proto/token/query";
 import {MetadataExt} from "../proto/token/token";
+
+export interface IMetadata extends Metadata{
+  denom_units: DenomUnit[];
+}
+
+export interface IMetadataExt extends MetadataExt{
+  max_supply: string;
+}
+
+interface QueryMetadataResponse {
+  metadata?: IMetadata;
+  metadata_ext?: IMetadataExt;
+  supply: string;
+}
 
 export class TokenAPI extends BaseAPI {
 
   public async apiTokenMetadatasByDenom(
     denom: string
-  ): Promise<QueryGetMetadataResponse> {
-    return this.request.get<QueryGetMetadataResponse>(
+  ): Promise<QueryMetadataResponse> {
+    return this.request.get<QueryMetadataResponse>(
       `/metaos-labs/metaos/token/metadatas/by_denom`,
       {
         denom
